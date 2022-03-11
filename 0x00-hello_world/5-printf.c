5cdd27b7ad956d94239dc60efa915d53cf520c21
George-9 <georgemuigainjau@gmail.com>
2022-03-11 19:20:56 +0300

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-	{
+{
 	printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
-	return 0;
-	}
+	return (0);
+}
