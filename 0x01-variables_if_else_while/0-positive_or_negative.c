c9dae2000f5215016ba2682e2a6dc88174a90b50
George-9 <georgemuigainjau@gmail.com>
2022-03-12 14:08:15 +0300

    C programming basic projects - Functions and format

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,12 @@ int n;
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
-
+	if(n==0)
+		printf("%i is 0\n", n);
 	if (n < 0)
-		printf("%i is negative \n", n);
+		printf("%i is negative\n", n);
 	else
-		printf("%i is positive \n", n);
+		printf("%i is positive\n", n);
 
 	return (0);
 }
