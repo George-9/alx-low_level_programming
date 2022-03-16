ef4e2f82d1b80a48405bf533377a81a0e4a4341e
George-9 <georgemuigainjau@gmail.com>
2022-03-16 17:17:09 +0300

    C function Prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,7 @@
 /**
 * print info prints some text to the standard output.
 * It's being called in sother programs.
+*char 'c' is a parameter  for the string to be printed to the
+*"stdio"
 */
 void printInfo(char *c);
