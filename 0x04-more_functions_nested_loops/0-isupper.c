4fb78c9083d51d60bbab69a6273a392ccfc8aec0
George-9 <georgemuigainjau@gmail.com>
2022-03-17 14:28:13 +0300

    C function Prototypes

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper - Returns a num 1 or 0 according to the checked character
+*
+* @c: only parameter
+*
+* Return: int
+*
+*/
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <ctype.h>
+
+/**
+* _isupper - Returns a num 1 or 0 according to the checked character
+*
+* @c: only parameter
+*
+* Return: int
+*
+*/
+int _isupper(int c);
