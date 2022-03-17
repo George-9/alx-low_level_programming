f7a614e163719f581fbbe12910725eca124afb36
George-9 <georgemuigainjau@gmail.com>
2022-03-17 13:18:47 +0300

    C function Prototypes

--- 0x02-functions_nested_loops/1-alphabet.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/**
-* print_alphabet - prints alphabets to stdio
-*
-* Return: void
-*/
-void print_alphabet(void);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,23 +4,6 @@
 #include <ctype.h>
 
 /**
-* main - Entry point
-*
-*
-* Return: always 0 (if Success)
-*/
-int main(void);
-
-/**
-* print info prints some text to the standard output.
-* It's being called in sother programs.
-* @c: (char) is a parameter  for the string to be printed to the
-*"stdio"
-* Return: char*
-*/
-void printInfo(char *c);
-
-/**
 * print_alphabet - print chars to stdio in lowercase.
 *
 * @void: parameter as void (no return)
