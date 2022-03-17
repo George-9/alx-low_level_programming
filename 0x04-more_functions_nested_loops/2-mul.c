f8cfd7bb858393a953e28a65e5d4660f4ab731f6
George-9 <georgemuigainjau@gmail.com>
2022-03-17 14:58:49 +0300

    C function Prototypes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - Returns a num 1 if c is a digit else 0
+*
+* @a: num parameter
+* @b: num param2
+*
+* Return: int
+*
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -20,3 +20,14 @@ int _isupper(int c);
 *
 */
 int _isdigit(int c);
+
+/**
+* mul - Returns a num 1 if c is a digit else 0
+*
+* @a: num parameter
+* @b: num param2
+*
+* Return: int
+*
+*/
+int mul(int a, int b);
