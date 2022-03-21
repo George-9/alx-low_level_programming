5951ff5113e2292377b8bb81dffa0c7d01a9044b
George-9 <georgemuigainjau@gmail.com>
2022-03-21 18:39:30 +0300

    strcpy in C

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
+/**
+* print_array - prints elements of array a
+*
+* @n: param1
+* @a: first param
+*
+* Return: void
+*/
 void print_array(int *a, int n)
 {
 	int len;
 
 	for (len = 0; len < n; len++)
 	{
+		if (len == sizeof(a))
+		return;
 		printf("%d, ", a[len]);
 	}
 	puts("\n");
