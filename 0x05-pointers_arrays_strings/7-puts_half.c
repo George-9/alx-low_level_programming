17ba95f08ee4d678937e9a8db986e559ab76e8a6
George-9 <georgemuigainjau@gmail.com>
2022-03-22 15:50:23 +0300

    printing each and every value in array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
+* puts_half - print half of a given string
 *
+* @str: param
 *
-*
-*
-*
+* Return: void
 *
 */
 void puts_half(char *str)
@@ -15,9 +15,10 @@ int length, looper;
 	length = strlen(str);
 	for (looper = 0; looper < length; looper++)
 	{
-		if(looper >= length / 2)
+		if (looper >= length / 2)
 		{
 			printf("%c", str[looper]);
 		}
 	}
+	putchar('\n');
 }
