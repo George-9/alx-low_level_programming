f8d3cc701833155430622a770cca376d493bd880
George-9 <georgemuigainjau@gmail.com>
2022-03-23 11:59:56 +0300

    concantenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strcat - concantenate two strings (dest and src)
+*
+* @dest: param 1
+* @src: source of the other string
+*
+* Return: char pointer
+*/
+char *_strcat(char *dest, char *src)
+{
+	return strcat(dest, src);
+}
