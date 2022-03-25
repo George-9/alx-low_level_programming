613e2375d165a94788448d6d23b01731f19474cd
George-9 <georgemuigainjau@gmail.com>
2022-03-25 16:06:33 +0300

    savee changes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,5 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	char *p = malloc(strlen(dest) + strlen(src) + 1);
-	int ptr = 0, temp = 0;
-
-	while (dest[temp] != '\0')
-	{
-		p[ptr++] = s[temp++];
-	}
-	temp = 0;
-	while (dest[temp] != '\0')
-	{
-		p[ptr++] = t[temp++];
-	}
-	return (p);
+	return (strcat(dest, src));
 }
