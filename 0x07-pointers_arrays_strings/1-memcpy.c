c95c860de8b7a79ab904ec2afbd85d6113c5ff2b
George-9 <georgemuigainjau@gmail.com>
2022-03-28 14:18:09 +0300

    memcopy in C programming

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
+* _memcpy - copy a memory address to another
 *
+* @dest: destination of copied bytes
+* @src: source of bytes
+* @n: n bytes
 *
-*
-*
-*
-*
+* Return: char
 */
-char _memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 char *tmp;
 
