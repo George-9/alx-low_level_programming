380b6b98ffdc5af1c801b4e6af141794525d1616
George-9 <georgemuigainjau@gmail.com>
2022-03-28 14:05:52 +0300

    memcopy in C programming

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*
+*
+*
+*/
+char _memcpy(char *dest, char *src, unsigned int n)
+{
+char *tmp;
+
+	memcpy(dest, src, strlen(tmp) + 1);
+	tmp = dest;
+	return (tmp);
+}
