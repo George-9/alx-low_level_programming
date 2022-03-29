bf3af6d4d08e860a127e0987d6ccd0cd5ea44d40
George-9 <georgemuigainjau@gmail.com>
2022-03-29 08:11:26 +0300

    pointers in C, memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,13 +7,12 @@
 * @b: char to fill
 * @n: times
 *
-* Return: char
+* Return: none
 */
-char *_memset(char *s, char b, unsigned int n)
+void *_memset(char *s, char b, unsigned int n)
 {
 	for (unsigned int i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-	return (s);
 }
