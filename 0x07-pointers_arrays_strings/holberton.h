0b0fb98702c3a660e7d089bad5b0e8bde1d968c0
George-9 <georgemuigainjau@gmail.com>
2022-03-29 07:12:32 +0300

    memset in C

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,9 +1,6 @@
 #ifndef FILE_HOLBERTON
 #define FILE_HOLBERTON
 
-#include <string.h>
-#include <stdlib.h>
-
 char *_memset(char *s, char b, unsigned int n);
 char _memcpy(char *dest, char *src, unsigned int n);
 
