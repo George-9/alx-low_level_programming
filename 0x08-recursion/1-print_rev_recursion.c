0dda45efe6f5e032e154f5fc071b315bab910dd8
George-9 <georgemuigainjau@gmail.com>
2022-03-30 15:39:56 +0300

    print a string recursively in C

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - prints a string recursively in reverse
+*
+* @s: string to iterate and print
+*
+* Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*c)
+	{
+		_print_rev_recursion(s + 1);
+	}
+	putchar(*s);
+}
