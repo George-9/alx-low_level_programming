524a5efc5746ba29a9a6b47f39dfa20148282063
George-9 <georgemuigainjau@gmail.com>
2022-04-01 12:24:00 +0300

    print arguments of a program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - main mfunction
+*
+* @argc: number of arguments for main
+* @argv: arguments for main
+*
+* Return - 0 if success (> 0) if error
+*/
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - main mfunction
+*
+* @argc: number of arguments for main
+* @argv: arguments for main
+*
+* Return - 0 if success (> 0) if error
+*/
+int main(int argc, char *argv[])
+{
+int looper;
+	while(argc--)
+	{
+		puts(*argv);
+		argv++;
+	}
+	return (0);
+}
+
