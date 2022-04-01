752945d98b93f1980e56c5ebcf496fdcbfe0e8c9
George-9 <georgemuigainjau@gmail.com>
2022-04-01 15:06:17 +0300

    print all commandline arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,11 +6,11 @@
 * @argc: number of arguments for main
 * @argv: arguments for main
 *
-* Return - 0 if success (> 0) if error
+* Return: 0 if success (> 0) if error
 */
 int main(int argc, char *argv[])
 {
-	while(argc--)
+	while (argc--)
 	{
 		puts(*argv);
 		argv++;
