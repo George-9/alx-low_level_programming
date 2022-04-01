0bb7f81f09299351ced9bbed53de6b90bf340d77
George-9 <georgemuigainjau@gmail.com>
2022-04-01 12:43:57 +0300

    manyplication using commandline arguments in C

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* main - main mfunction
+*
+* @argc: number of arguments for main
+* @argv: arguments for main
+*
+* Return - 0 if success (> 0) if error
+*/
+int main(int argc, char *argv[])
+{
+int count;
+	if (!argv[1] || !argv[2])
+	{
+	puts("error");
+	return 1;
+	}
+	else
+	{
+		count = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", count);
+	}
+	return (0);
+}
