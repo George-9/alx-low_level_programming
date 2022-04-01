35a708e14a9687e98ad1a736d7ba810fb80f69d6
George-9 <georgemuigainjau@gmail.com>
2022-04-01 12:05:14 +0300

    commandline args; print name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - main mfunction
+*
+* @argc: number of arguments for main
+* @argv: arguments for main
+*
+* Return - 0 if success (> 0) if error
+*/
+int main(int argc, char *argv[])
+{
+char *a;
+	a = argv[0];
+	puts(a);
+	return (0);
+}
