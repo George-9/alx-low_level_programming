af994320ed0e1745aed46f1c0ef101a7a805f8da
George-9 <georgemuigainjau@gmail.com>
2022-05-10 17:32:41 +0300

    appending content to files in C

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,17 +3,19 @@
 #include "main.h"
 
 /**
-* append-text_to_file - appends text to agiven file
+* append_text_to_file - appends text to agiven file
 *
-* @opened: check if opening the file and appending was a success
+* @filename: name of file to append data to
+* @text_content: content to append to the file
 *
-*
-*
+* Return: depends on existence of file or
+* success to appending to file [1] or [-1]
 */
-int append-text_to_file(char *filename, char *text_content)
+int append_text_to_file(char *filename, char *text_content)
 {
 	FILE *check;
 	int opened;
+	int checkExists;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,15 +25,18 @@ int append-text_to_file(char *filename, char *text_content)
 
 	opened = fopen(filename, "a+");
 
-	if(opened = -1)
+	if ((opened == -1))
 		return (-1);
 
-	if ((check = fopen(filename, "r"))
+	checkExists = (check = fopen(filename, "r"));
+
+	if (checkExists)
 	{
 		fputs(text_content, opened);
 		fclose(opened);
 
 		return (1);
-	}else
+	}
+	else
 		return (-1);
 }
