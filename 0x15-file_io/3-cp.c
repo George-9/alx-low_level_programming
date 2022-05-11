9715a9bac937281a946f437a7d3a725f037df26c
George-9 <georgemuigainjau@gmail.com>
2022-05-11 13:50:34 +0300

    copy file contents to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,6 @@
 * @argv: string argument
 * Return: 0
 */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to;
@@ -17,15 +16,17 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	
+
 	file_from = open(argv[1], O_RDONLY);
-	
+
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Cant read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
+
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Cant write to %s\n", argv[2]);
@@ -45,11 +46,13 @@ int main(int argc, char *argv[])
 	}
 
 	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", file_from), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", file_from);
+		exit(100);
+	}
 
 	if (close(file_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", file_to), exit(100);
 
 	return (0);
 }
-
