9c1c31783c8f678ee084ef24a3f694badf7e39ae
George-9 <georgemuigainjau@gmail.com>
2022-05-11 07:49:31 +0300

    appending data to files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
-* append_text_to_file - appends text to agiven file
-*
-* @filename: name of file to append data to
-* @text_content: content to append to the file
-*
-* Return: depends on existence of file or
-* success to appending to file [1] or [-1]
-*/
-int append_text_to_file(char *filename, char *text_content)
+ * append_text_to_file - that appends text at the end of a file
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: function that appends text at the end of a file
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int opened = 0, file;
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,16 +20,18 @@ int append_text_to_file(char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	while (text_content[opened] != '\0')
+
+	while (text_content[i] != '\0')
 	{
-		opened++;
+		i++;
 	}
-	file = fopen(filename, O_WRONLY | O_APPEND);
+
+	file = open(filename, O_WRONLY | O_APPEND);
 
 	if (file == -1)
 		return (-1);
 
-	write_file(file, text_content, opened);
+	write(file, text_content, i);
 
 	return (1);
 }
