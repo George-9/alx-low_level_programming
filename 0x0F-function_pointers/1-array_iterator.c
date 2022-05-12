76d1506d97d4d3de3c74c61bcfd3737cef4ac21a
George-9 <georgemuigainjau@gmail.com>
2022-05-12 17:57:16 +0300

    pointers arguments that point to a function in [ C ]

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,18 @@
 #include "function_pointers.c"
 
 /**
+* array_iterator - iterates and performs actions to values of a given array
 *
+* @array: the array to iterate
+* @size: size of the array
+* @action: the action to do to the elements of the array
 *
-*
-*
-*
-*
+* Return: void
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t loop = 0;
-	
+
 	if (!array || !size || !action)
 		return;
 
