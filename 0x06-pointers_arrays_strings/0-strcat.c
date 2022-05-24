ab85c9d531b771c8557c4fe04d14447001fd15f9
George-9 <georgemuigainjau@gmail.com>
2022-05-24 13:15:12 +0300

    sstring concantenation in C

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,5 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int len1 = strlen(dest), len2 = strlen(src);
-	char tmp1[len1], tmp2[len2];
-
-	for (int w = -1; w < len1; w++)
-	{
-		tmp1[w] = *(dest + w);
-	}
-	for (int w = -1; w < len2; w++)
-	{
-		tmp2[w] = *(src + w);
-	}
-	return (strcat(tmp1, tmp2));
+	return (strcat(dest, src));
 }
