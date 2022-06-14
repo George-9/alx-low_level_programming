a02245bc489008260a53ec52e2c1697811959a4b
George-9 <georgemuigainjau@gmail.com>
2022-06-14 13:33:55 +0300

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,12 @@
+#ifndef FILE_DOG
+#define FILE_DOG
 /**
- * sruct dog - a dog structure
- * 
+ * sruct dog - a small dog structure
  * @name: (first member) name of the dog
  * @age: (second member) age of the dog
  * @owner: (third member) name of the dog's owner
  *
+ * Description: a dog structure [holds the dog's name, age and owner's name]
  */
 struct dog
 {
@@ -12,3 +14,4 @@ struct dog
 	float age;
 	char *owner;
 }
+#endif
