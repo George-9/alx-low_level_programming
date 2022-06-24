facc5ef33ac76717b6c6685fea72ada908bb5122
George-9 <georgemuigainjau@gmail.com>
2022-06-24 18:54:42 +0300

    header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,31 +3,12 @@
 #include <string.h>
 #include <ctype.h>
 
-/**
-* printInfo - print info to the console
-*
-* @c: parameter
-*
-* Return: void
-*/
+int _putchar(char c);
+
 void printInfo(char *c);
 
-/**
-* print_alphabet - print chars to stdio in lowercase.
-*
-* @void: parameter as void (no return)
-*
-* Return: void
-*/
 void print_alphabet(void);
 
-/**
-* print_alphabet_x10 - print alphabets 10 times
-*
-*
-* Return: void
-*/
 void print_alphabet_x10(void);
-
 
 int _islower(int c);
