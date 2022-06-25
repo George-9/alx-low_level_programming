dd6d49d9873bb68bddbb688ae92b55cba301526f
George-9 <georgemuigainjau@gmail.com>
2022-06-25 08:46:56 +0300

    a function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+/**
+* print_sign - chaecks for n if positive, negative or a 0
+*
+* @n: first and only parameter (one to be checked)
+*
+* Description: prints "0" if n is zero "-" if n is negative otherwise "1"
+*
+* Return: -1 if n is negative 0 if n is 0 or 1 if n is positive(greater than zero)
+*/
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	_putchar('+');
+	return (1);
+}
