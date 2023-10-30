a32648835ad0978dca9514068018fdc3ecb8607a
George-9 <georgemuigainjau@gmail.com>
2023-10-30 13:06:36 +0300

    modulus and conditionals

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,15 +10,18 @@
 int main(void)
 {
 	int n, ld;
-	char *msg, *msg_tmp1, *msg_tmp2;
+	char *msg;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	ld = n < 0 ? -n % 10 : n % 10;
+	ld = n < 0 ? n % 10 : n % 10;
 
-	msg_tmp1 = " and is greater than 5";
-	msg_tmp2 = " and is less than 6 and not 0";
-	msg = ld > 5 ? msg_tmp1 : n == 0 ? "and is zero" : msg_tmp2;
+	if (ld == 0)
+		msg = " and is zero";
+	else if (ld < 6 && ld != 0)
+		msg = " and is less than 6 and not 0";
+	else
+		msg = " and is greater than 5";
 
 	printf("Last digit of ");
 	printf("%d %s %d", n, "is", ld);
