4280325387e8e591a80e20e4ba6ad2f8aa5ef192
George-9 <georgemuigainjau@gmail.com>
2023-11-08 12:01:59 +0300

    header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,18 +1,6 @@
 #include <stdio.h>
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _isupper(int c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-void print_to_98(int n);
-void print_number(int n);
 int add(int, int);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void); 
