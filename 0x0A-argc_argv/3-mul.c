c8124d2ee8385934d3ba1f1df323efa4d8d91a6f
George-9 <georgemuigainjau@gmail.com>
2023-11-17 19:47:56 +0300

    header file updates

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - program main entry point
+ * @argv: the cmd line arguments of the program
+ * @argc: the number of argumnets
+ *
+ * Return: 0 if no error
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+		return (-1);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
