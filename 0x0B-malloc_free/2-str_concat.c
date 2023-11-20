9d83c48ac00c94612509f1bbcc111fda9a89a153
George-9 <georgemuigainjau@gmail.com>
2023-11-20 09:59:51 +0300

    a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,28 +10,35 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *n;
-	int i, l, l1, l2;
+	size_t i, l;
 
-	if (s1 != NULL)
-		l1 = strlen(s1);
-	else
-		l1 = 0;
-
-	if (s2 != NULL)
-		l2 = strlen(s2);
-	else
-		l2 = 0;
-
-	n = malloc(l1 + l2 + 1);
-	if (n == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
-	if (s1 != NULL)
-		for (l = 0; l < l1; l++)
-			n[l] = s1[l];
-	if (s2 != NULL)
-		for (i = 0; l < l1 + l2; l++, i++)
-			n[l] = s2[i];
+	if (s1 != NULL && s2 == NULL)
+	{
+		n = malloc((sizeof(char) * strlen(s1)) + 1);
+		if (n == NULL)
+			return (NULL);
+		for (i = 0; i < strlen(s1); i++)
+			n[i] = s1[i];
+		return (n);
+	}
+	if (s1 == NULL && s2 != NULL)
+	{
+		n = malloc((sizeof(char) * strlen(s2)) + 1);
+		if (n == NULL)
+			return (NULL);
+		for (i = 0; i < strlen(s2); i++)
+			n[i] = s2[i];
+		return (n);
+	}
+
+	n = malloc((sizeof(char) * (strlen(s1) + strlen(s2))) + 1);
+	for (i = 0; i < strlen(s1); i++)
+		n[i] = s1[i];
+	for (l = 0; l < strlen(s2); l++, i++)
+		n[i] = s2[i];
 
 	return (n);
 }
