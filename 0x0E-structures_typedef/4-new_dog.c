49d475b7a0d4f1a2c4b11251baa7346914390678
George-9 <georgemuigainjau@gmail.com>
2023-11-27 16:18:10 +0300

    header file

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog object
+ *
+ * @name: the name to assign to it
+ * @age: the age to assing to the dog
+ * @owner: the name of the new dog's owner
+ *
+ * Return: a pointer to the new dog object
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /**
  * struct dog - definesa dog object
@@ -21,5 +21,6 @@ typedef struct dog
 
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
