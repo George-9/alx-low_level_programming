3c06473c514222c1cca3b19767a54ed15a7eff77
George-9 <georgemuigainjau@gmail.com>
2023-12-01 09:49:04 +0300

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,8 +1,5 @@
 #include "variadic_functions.h"
 
-int _putchar(char c);
-void print_number(int);
-
 /**
  * print_numbers - prints numbers
  * @separator: character printed between the numbers
@@ -13,55 +10,22 @@ void print_number(int);
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i, l, r;
+	unsigned int i, l;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		l = va_arg(args, int);
-		print_number(l);
+		printf("%i", l);
+
 		if ((i + 1) != n)
 		{
-			for (r = 0; separator[r] != '\0'; r++)
-				_putchar(separator[r]);
+			if (separator == NULL)
+				printf(" ");
+			else
+				printf("%s", separator);
 		}
 	}
 	va_end(args);
-	_putchar('\n');
-}
-
-/**
- * print_number - prints a number
- * @n: the number
- *
- * Return: none
- */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	if ((n / 10) != 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+	printf("%c", '\n');
 }
