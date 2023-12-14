a46e311fe310c0d23f570722828997b2531e15f9
George-9 <georgemuigainjau@gmail.com>
2023-12-14 11:10:36 +0300

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (n > 0)
+	{
+		_putchar((n & 1) + '0');
+		n >>= 1;
+	}
+}
