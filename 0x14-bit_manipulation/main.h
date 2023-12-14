d0f2324a99a4ddcc0d04b770742ebc38434d2c9e
George-9 <georgemuigainjau@gmail.com>
2023-12-14 11:11:00 +0300

    header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <math.h>
 
+int _putchar(int c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
