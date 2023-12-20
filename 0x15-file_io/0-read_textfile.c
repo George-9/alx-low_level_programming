4ba4353b53c5f305d4ff75422af43cde9b3e0206
George-9 <georgemuigainjau@gmail.com>
2023-12-20 09:27:29 +0300

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads text from a file
+ * @filename: the file to read
+ * @letters: the number of characters to read from the file
+ *
+ * Return: numbers of letters read, otherwise 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file = fopen(filename, "r");
+	char c;
+	size_t count = 0;
+
+	if (!file)
+		return (0);
+
+	do {
+		c = fgetc(file);
+		_putchar(c);
+		count++;
+	} while ((c != EOF) && (count < letters - 1));
+
+	return (count);
+}
